fd711f6a82aae3842b9d3938ce048955509b7b3f
Avani Singh <avani.singh@BLRETV-QLQ399JQ.local>
2022-06-20 13:18:50 +0530

    rename services to service

--- CRS-8-Flipkart-POS/src/com/flipkart/service/StudentImpl.java
+++ CRS-8-Flipkart-POS/src/com/flipkart/service/StudentImpl.java
@@ -1,6 +1,10 @@
 package com.flipkart.service;
 
-public class StudentImpl {
+public class StudentImpl implements StudentInterface  {
 
+    public void viewGrades(String studentID, String password){};
+    public void register(String name, String studentID, String branch, String address, String password){};
+    public void addCourse(String studentID, String CourseCode){};
+    public void dropCourse(String studentID, String CourseCode){};
 
 }
