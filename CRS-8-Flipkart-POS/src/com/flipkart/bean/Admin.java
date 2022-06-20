0a5b804a0313cfe1b6815a838e3961d86a90f07e
Anmol Gupta <anmol.gupta4@BLRETV-254PG1H4.local>
2022-06-20 12:46:47 +0530

    AdminOperation added

--- CRS-8-Flipkart-POS/src/com/flipkart/bean/Admin.java
+++ CRS-8-Flipkart-POS/src/com/flipkart/bean/Admin.java
@@ -2,15 +2,27 @@ package com.flipkart.bean;
 
 import java.util.Date;
 
-/
+/**
+ *
+ * @author JEDI Group 8
+ * Admin Class
+ *
+ */
 public class Admin extends User{
     private Date dateOfJoining;
 
-
+    /**
+     * Method to get Date of joining
+     * @return Date of joining
+     */
     public Date getDateOfJoining() {
         return dateOfJoining;
     }
 
+    /**
+     * Method to set Date of joining
+     * @param dateOfJoining
+     */
     public void setDateOfJoining(Date dateOfJoining) {
         this.dateOfJoining = dateOfJoining;
     }
