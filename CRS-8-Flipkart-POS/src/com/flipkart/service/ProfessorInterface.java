24f414e75a56bee6cf2abf642c68bad062889f8d
Mohan Joshi <mohan.joshi@flipkart.com>
2022-06-20 13:38:37 +0530

    changes only

--- CRS-8-Flipkart-POS/src/com/flipkart/service/ProfessorInterface.java
+++ CRS-8-Flipkart-POS/src/com/flipkart/service/ProfessorInterface.java
@@ -1,48 +1,14 @@
 package com.flipkart.service;
 
-import com.flipkart.bean.Course;
-import com.flipkart.bean.EnrolledStudent;
-import com.flipkart.exception.GradeNotAddedException;
 
-import java.sql.SQLException;
-import java.util.List;
 
-/**
- *
- * @author JEDI Group 8
- * Interface for Professor Operations
- *
- */
 public interface ProfessorInterface {
 
-    /**
-     * Method to grade a Student
-     * @param studentId
-     * @param courseCode
-     * @param grade
-     * @return boolean indicating if grade is added or not
-     * @throws GradeNotAddedException
-     */
-    public boolean addGrade(int studentId,String courseCode,String grade) throws GradeNotAddedException;
+    public boolean addGrade(int studentId,String courseCode,String grade);
 
-    /**
-     * Method to view all the enrolled students
-     * @param profId: professor id
-     * @return List of enrolled students
-     */
-    public List<EnrolledStudent> viewEnrolledStudents(String profId) throws SQLException;
+    public List<EnrolledStudent> viewEnrolledStudents(String profId) ;
 
-    /**
-     * Method to get list of all course a professor is teaching
-     * @param profId: professor id
-     * @return List of courses the professor is teaching
-     */
     public List<Course> getCourses(String profId);
 
-    /**
-     * Method to get the professor name with ID
-     * @param profId
-     * @return Professor name
-     */
     public String getProfessorById(String profId);
 }
