3519642c53632709ba9c39f5162686f99148fce7
Paras Joshi <paras.joshi@BLRETV-RT6HT2DW.local>
2022-06-21 13:34:56 +0530

    Changes

--- CRS-8-Flipkart-POS/src/com/flipkart/service/StudentImpl.java
+++ CRS-8-Flipkart-POS/src/com/flipkart/service/StudentImpl.java
@@ -3,8 +3,13 @@ package com.flipkart.service;
 public class StudentImpl implements StudentInterface  {
 
     public void viewGrades(String studentID, String password){};
-    public void register(String name, String studentID, String branch, String address, String password){};
+    public int register(String name, String studentID, String password, String branch, int batch, String address, String country){
+        return 1;
+    };
     public void addCourse(String studentID, String CourseCode){};
     public void dropCourse(String studentID, String CourseCode){};
+
+
+
 
 }
