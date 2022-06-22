d36b4ef0e06fe3b6c9992d6e05a994b4f7be39a0
Srijan Sarkar <srijan.sarkar@BLRETV-Y73T5XVF.local>
2022-06-22 17:30:30 +0530

    added professor dao interface

---
+++ CRS-8-Flipkart-POS/src/com/flipkart/dao/ProfessorDaoInterface.java
@@ -0,0 +1,18 @@
+package com.flipkart.dao;
+
+
+import com.flipkart.bean.Course;
+import com.flipkart.bean.EnrolledStudent;
+
+
+import java.util.List;
+
+public interface ProfessorDaoInterface {
+    public boolean addGrade(String studentId);
+
+    public List<EnrolledStudent> viewEnrolledStudents(String profId);
+
+    public List<Course> getCourses(String profId);
+
+    public String getProfessorById(String profId);
+}
