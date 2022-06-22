7396dd4f0c11aafede8bbd9035a81dcddea1e345
RitsPar123 <107409313+RitsPar123@users.noreply.github.com>
2022-06-22 17:55:50 +0530

    Add files via upload

---
+++ CRS-8-Flipkart-POS/src/com/flipkart/dao/StudentDaoInterface.java
@@ -0,0 +1,19 @@
+package com.flipkart.dao;
+
+import com.flipkart.constant.Gender;
+import com.flipkart.constant.Role;
+
+public interface StudentDaoInterface {
+    public void viewGrades(String studentID);
+
+    public String register(String name, String studentID, String password, Role role, Gender gender, String branch, int batch, String address, String country) ;
+
+    public void addCourse(String studentID);
+    public void dropCourse(String studentID);
+
+    public void viewRegisteredCourses(String studentID);
+    public void payFees(String studentID);
+    public void showNotifications(String studentID);
+
+    public boolean isApproved(String studentId);
+}
