9f6633ea0c337a93beb7961a5b34f39c51fe136d
Mohan Joshi <mohan.joshi@flipkart.com>
2022-06-23 15:33:39 +0530

    Sql query constants added for student dao operation

--- CRS-8-Flipkart-POS/src/com/flipkart/constant/SQLQueryConstants.java
+++ CRS-8-Flipkart-POS/src/com/flipkart/constant/SQLQueryConstants.java
@@ -5,4 +5,12 @@ public class SQLQueryConstants {
     //admin dao
     public static final String ADD_COURSE_QUERY = "insert into coursecatalog values (?, ?, ?, ?, ?);";
     public static final String VIEW_COURSES_IN_CATALOG_QUERY = "select * from coursecatalog";
+
+
+    //student operations
+    public static final String VIEW_GRADE_QUERY= "select courseId, grade from studentcoursegrade where studentId = ? ;";
+    public static final String VIEW_REGISTERED_COURSE_QUERY="select courseId from studentcoursegrade where studentId = ?;";
+    public static final String IS_STUDENT_APPROVED="select isApproved from student where studentId = ?;";
+    public static final String ADD_STUDENT_PAYMENT_QUERY="insert into payment values(?, ?, ?, ?, ?);";
+
 }
