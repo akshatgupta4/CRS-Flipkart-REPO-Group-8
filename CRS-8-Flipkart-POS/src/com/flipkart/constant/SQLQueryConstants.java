3a1c02a251e6ec64b33c7dad227e959d2e217b62
Avani Singh <avani.singh@192.168.1.5>
2022-06-23 15:54:05 +0530

    Added getEnrolledStudents in ProfDaoOp

--- CRS-8-Flipkart-POS/src/com/flipkart/constant/SQLQueryConstants.java
+++ CRS-8-Flipkart-POS/src/com/flipkart/constant/SQLQueryConstants.java
@@ -4,4 +4,9 @@ public class SQLQueryConstants {
 
     //admin dao
     public static final String ADD_COURSE_QUERY = "insert into coursecatalog values (?, ?, ?, ?, ?);";
+
+    //professor dao
+    public static final String GET_ENROLLED_STUDENTS="select studentId from studentcoursegrade inner join course on studentcoursegrade.courseId = course.courseID and profId=?";
+
+
 }
