0302c2bc6bf3fc6a8d0fb8af964a5de5855c6f39
Mohan Joshi <mohan.joshi@flipkart.com>
2022-06-24 12:51:23 +0530

    UserDao done

--- CRS-8-Flipkart-POS/src/com/flipkart/util/CRSDbConnection.java
+++ CRS-8-Flipkart-POS/src/com/flipkart/util/CRSDbConnection.java
@@ -13,7 +13,7 @@ public class CRSDbConnection {
 
     //  Database credentials
     static final String USER = "root";
-    static final String PASS = "password";
+    static final String PASS = "hellojoshi123";
 
 
     public static Connection getConnection() {
@@ -23,7 +23,7 @@ public class CRSDbConnection {
                     return connection;
                 }
                 else {
-                    Class.forName("com.mysql.jdbc.Driver");
+                    Class.forName(JDBC_DRIVER);
                     connection = DriverManager.getConnection(DB_URL,USER,PASS);
     //                Properties prop = new Properties();
     //                InputStream inputStream = CRSDb.class.getClassLoader().getResourceAsStream("./config.properties");
