aa787775630808c79871b58a449420b85fcfc213
Akshat Gupta <akshat.gupta4@BLRETV-RDPF4FQ0.local>
2022-06-24 13:20:55 +0530

    added comments to student interface

--- CRS-8-Flipkart-POS/src/com/flipkart/dao/AdminDaoOperation.java
+++ CRS-8-Flipkart-POS/src/com/flipkart/dao/AdminDaoOperation.java
@@ -88,7 +88,6 @@ public class AdminDaoOperation implements AdminDaoInterface {
             connection.close();
         }
 
-
     }
 
 
--- CRS-8-Flipkart-POS/src/com/flipkart/service/StudentInterface.java
+++ CRS-8-Flipkart-POS/src/com/flipkart/service/StudentInterface.java
@@ -5,16 +5,63 @@ import com.flipkart.constant.Role;
 
 public interface StudentInterface {
 
+    /**
+     * Method to view grades of a student
+     * @param studentID
+     */
     public void viewGrades(String studentID);
 
+    /**
+     * Method to register student into the system
+     * @param name
+     * @param studentID
+     * @param password
+     * @param role
+     * @param gender
+     * @param branch
+     * @param batch
+     * @param address
+     * @param country
+     * @return student id
+     */
     public String register(String name, String studentID, String password, Role role, Gender gender, String branch, int batch, String address, String country) ;
 
+    /**
+     * Method to add course to student's registered courses
+     * @param studentID
+     */
     public void addCourse(String studentID);
+
+    /**
+     * Method to remove course from student's registered courses
+     * @param studentID
+     */
     public void dropCourse(String studentID);
 
+    /**
+     * Method to view registered courses of a student
+     * @param studentID
+     */
+
     public void viewRegisteredCourses(String studentID);
+
+    /**
+     * Method to implement fee payment for a student
+     * @param studentID
+     */
     public void payFees(String studentID);
+
+    /**
+     * Method to display notifications
+     * @param studentID
+     */
     public void showNotifications(String studentID);
+
+    /**
+     * Method to check if a student is approved
+     * @param studentId
+     * @return
+     */
 
     public boolean isApproved(String studentId);
 
