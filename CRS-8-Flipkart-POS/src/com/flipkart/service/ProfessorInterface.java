6f12bd492c0d8d62153710f86a0fdff92d3fd9a4
Akshat Gupta <akshat.gupta4@BLRETV-RDPF4FQ0.local>
2022-06-24 13:49:00 +0530

    added exceptions

--- CRS-8-Flipkart-POS/src/com/flipkart/service/ProfessorInterface.java
+++ CRS-8-Flipkart-POS/src/com/flipkart/service/ProfessorInterface.java
@@ -8,16 +8,19 @@ import java.util.List;
 
 import com.flipkart.bean.Course;
 import com.flipkart.bean.EnrolledStudent;
+import com.flipkart.exception.CourseNotFoundException;
+import com.flipkart.exception.ProfessorDoesNotExistsException;
+import com.flipkart.exception.UserNotFoundException;
 
 import java.util.List;
 
 public interface ProfessorInterface {
 
-    public boolean addGrade(String studentId, String courseId, String grade);
+    public boolean addGrade(String studentId, String courseId, String grade) throws UserNotFoundException, SQLException, CourseNotFoundException;
 
     public List<EnrolledStudent> viewEnrolledStudents(String profId) throws SQLException;
 
-    public List<Course> getCourses(String profId) throws SQLException;
+    public List<Course> getCourses(String profId) throws SQLException, ProfessorDoesNotExistsException;
 
     public String getProfessorById(String profId);
 }
